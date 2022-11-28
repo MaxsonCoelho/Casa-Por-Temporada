767a21700466ce46e6e2baa99d7fddec18b792a0
Maxson Coelho <maxsoncoelho@gmail.com>
2022-11-28 08:26:12 -0400

    initial project

---
+++ app/src/main/java/com/maxsoncoelho/casaportemporada/activity/autenticacao/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.maxsoncoelho.casaportemporada.activity.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maxsoncoelho.casaportemporada.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
